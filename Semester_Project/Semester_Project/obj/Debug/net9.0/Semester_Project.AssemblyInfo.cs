f2fa94fa475743ed77a0f81bad0b81d0668f43c0
Muhammad Aqib <muhammadaqibamjad@gmail.com>
2025-12-10 18:01:20 +0500

    feat: add initial build output and cache files for Semester_Project.

--- Semester_Project/Semester_Project/obj/Debug/net9.0/Semester_Project.AssemblyInfo.cs
+++ Semester_Project/Semester_Project/obj/Debug/net9.0/Semester_Project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66bcf782119e45d015acb320e67ef36bdcffcee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468949b351187f0cd92657f65a59a33306d156ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
