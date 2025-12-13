40cde181b7bbad71a971376c58ef66dfbaeac27e
Muhammad Aqib <muhammadaqibamjad@gmail.com>
2025-12-13 10:34:56 +0500

    feat: add ISP dashboard view with customer and revenue metrics, and introduce a login partial.

--- Semester_Project/Semester_Project/obj/Debug/net9.0/Semester_Project.AssemblyInfo.cs
+++ Semester_Project/Semester_Project/obj/Debug/net9.0/Semester_Project.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874d5727735b11e36a7087cf1ae71612173e90c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29eda19d32aa400153288aff7d29556163d55ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
